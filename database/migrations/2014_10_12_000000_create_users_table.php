f0c73d2fc3bcc7bd20df4f293bc6b32b0ce1d2f4
davidralphm <74144561+davidralphm@users.noreply.github.com>
2023-11-14 17:08:26 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username', 255)->unique();
-            $table->string('first_name', 255);
-            $table->string('last_name', 255);
+            $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('status')->default(0);
-            $table->string('signature', 255)->nullable();
-            $table->string('image_url', 255)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
