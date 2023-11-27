169fd27a41535b1987fad76a7d471e17d500b50c
naufalqii16 <naufalqii16@gmail.com>
2023-11-27 15:24:13 +0700

    add fe login and register

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -3,7 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        .gradient-custom {
+        /* fallback for old browsers */
+        background: #6a11cb;
+
+        /* Chrome 10-25, Safari 5.1-6 */
+        background: -webkit-linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
+
+        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))
+        }
+    </style>
 </head>
 <body>
     @if ($errors->any())
@@ -16,13 +29,59 @@
         <b>{{ Session::get('message-success') }}</b>
     @endif
 
-    <form action="/login" method="post">
+    {{-- <form action="/login" method="post">
         {{ csrf_field() }}
 
         <input type="email" name="email" id="email" placeholder="email" value="{{ old('email') }}">
         <input type="password" name="password" id="password" placeholder="password">
 
         <input type="submit" value="Login">
-    </form>
+    </form> --}}
+
+    {{-- ===================================================================================== --}}
+    <section class="vh-100 gradient-custom">
+        <div class="container py-3 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                <div class="card-body p-5 text-center">
+
+                  <div class="mb-md-5 mt-md-4 pb-5">
+
+                    <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                    <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                    <form action="/login" method="post">
+                        {{ csrf_field() }}
+                            <div class="form-outline form-white mb-4">
+                                <input type="email" name="email" id="email" placeholder="email" value="{{ old('email') }}" class="form-control form-control-lg">
+                            </div>
+
+                            <div class="form-outline form-white mb-4">
+                                <input type="password" name="password" id="password" placeholder="password" class="form-control form-control-lg" >
+                            </div>
+
+                            <input type="submit" value="Login" class="btn btn-outline-light btn-lg px-5">
+
+                    </form>
+
+                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                    </div>
+
+                  </div>
+
+                  <div>
+                    <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+                    </p>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 </body>
 </html>
